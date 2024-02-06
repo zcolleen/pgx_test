aba02723c6f72a2bb2142f82e24e96a3b8eec690
sleygin <sleygin@avito.ru>
2024-02-06 23:19:40 +0300

    example

--- go.mod
+++ go.mod
@@ -8,9 +8,9 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
 
-replace github.com/jackc/pgx/v5 => github.com/zcolleen/pgx/v5 v5.0.0-20231220223345-731a8c2a6611
+replace github.com/jackc/pgx/v5 => ../pgx
--- main.go
+++ main.go
@@ -2,160 +2,52 @@ package main
 
 import (
 	"context"
-	"errors"
 	"log"
-	"net/http"
-	"strconv"
-	"sync/atomic"
 	"time"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgconn/ctxwatch"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
-
-type WorkerPool struct {
-	workerNum int
-	tasks     chan func()
-}
-
-func NewWorkerPool(ctx context.Context, workerNum int) *WorkerPool {
-	w := &WorkerPool{
-		tasks: make(chan func(), workerNum),
-	}
-	for i := 0; i < workerNum; i++ {
-		go func() {
-			for {
-				select {
-				case t := <-w.tasks:
-					t()
-				case <-ctx.Done():
-					return
-				}
-			}
-		}()
-	}
-
-	return w
-}
-
-func (w *WorkerPool) Exec(f func()) {
-	w.tasks <- f
-}
-
-func execTx(ctx context.Context, pool *pgxpool.Pool) (err error) {
-	tx, err := pool.Begin(ctx)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if errTx := tx.Rollback(ctx); errTx != nil {
-			if !errors.Is(errTx, pgx.ErrTxClosed) {
-				err = errTx
-			}
-		}
-	}()
-
-	_, err = tx.Exec(ctx, "select pg_sleep(0.5)")
-	if err != nil {
-		return err
-	}
-
-	ctx, cancel := context.WithCancel(ctx)
-	cancel()
-
-	return tx.Commit(ctx)
-}
-
-func exec(ctx context.Context, pool *pgxpool.Pool) error {
-	r, err := pool.Query(ctx, "select * from pgx_table limit 10")
-	if err != nil {
-		return err
-	}
-	var id, age int
-	var name, addr string
-	for r.Next() {
-		if err := r.Scan(&id, &name, &age, &addr); err != nil {
-			return err
-		}
-	}
-
-	return r.Err()
-}
-
-func metrics(a *atomic.Uint64, failed *atomic.Uint64, deadlineFailures *atomic.Uint64) {
-	http.HandleFunc("/total", func(writer http.ResponseWriter, request *http.Request) {
-		res := strconv.FormatUint(a.Load(), 10)
-
-		writer.Write([]byte(res))
-	})
-
-	http.HandleFunc("/failed", func(writer http.ResponseWriter, request *http.Request) {
-		res := strconv.FormatUint(failed.Load(), 10)
-
-		writer.Write([]byte(res))
-	})
-
-	http.HandleFunc("/rps", func(writer http.ResponseWriter, request *http.Request) {
-		val1 := a.Load()
-		time.Sleep(time.Second)
-		val2 := a.Load()
-
-		res := strconv.FormatUint(val2-val1, 10)
-		writer.Write([]byte(res))
-	})
-
-	http.HandleFunc("/failed/deadline", func(writer http.ResponseWriter, request *http.Request) {
-		total := a.Load()
-		dFailed := deadlineFailures.Load()
-
-		persentage := float64(dFailed) / float64(total)
-		res := strconv.FormatFloat(persentage, 'g', 10, 64)
-
-		writer.Write([]byte(res))
-	})
-
-	go func() {
-		panic(http.ListenAndServe("0.0.0.0:8081", nil))
-	}()
-}
 
 func main() {
 	ctx := context.Background()
 
-	pool, err := pgxpool.New(ctx, "postgres://dbuser:dbpassword@192.168.1.3:5432/postgres?sslmode=disable&default_query_exec_mode=simple_protocol&no_closing_conn_mode=true&conn_cleanup_timeout=200s")
+	cfg, err := pgxpool.ParseConfig("postgres://dbuser:dbpassword@localhost:5432/postgres?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
 
-	total := &atomic.Uint64{}
-	deadlineFailures := &atomic.Uint64{}
-	failed := &atomic.Uint64{}
-	metrics(total, failed, deadlineFailures)
-
-	w := NewWorkerPool(ctx, 11)
-
-	f := func() {
-		defer total.Add(1)
-
-		c, cancel := context.WithTimeout(ctx, time.Millisecond*20)
-		defer cancel()
-
-		err = exec(c, pool)
-		if err != nil {
-
-			failed.Add(1)
-			if errors.Is(err, context.DeadlineExceeded) {
-				deadlineFailures.Add(1)
-			} else {
-				log.Println(err)
-			}
-
-			return
+	cfg.ConnConfig.BuildContextWatcherHandler = func(conn *pgconn.PgConn) ctxwatch.Handler {
+		return &pgconn.DeadlineContextWatcherHandler{
+			Conn: conn.Conn(),
+			// long enough delay to let query finish
+			DeadlineDelay: time.Second,
 		}
+	}
 
-		total.Add(1)
+	pool, err := pgxpool.NewWithConfig(ctx, cfg)
+	if err != nil {
+		panic(err)
 	}
-	for {
-		w.Exec(f)
+
+	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*100)
+	defer cancel()
+
+	rows, err := pool.Query(ctx, "select pg_sleep(0.2)")
+	// no error here
+	if err != nil {
+		panic(err)
 	}
+	defer rows.Close()
+
+	for rows.Next() {
+	}
+
+	// still no error while it should be because context is already canceled
+	// in unchanged Handler there will be context deadline exceeded error
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+	log.Println("finished without errors")
 }
