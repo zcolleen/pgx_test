8a9353076e4b9ba68a2b586379bb5f478e17c470
sleygin <sleygin@avito.ru>
2024-02-10 02:56:39 +0300

    example

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"log"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgconn"
@@ -10,9 +9,56 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func main() {
-	ctx := context.Background()
+type handler struct {
+	pool *pgxpool.Pool
+}
 
+// some long operation or some external call
+func someLongMathOperationOrSomeLongExternalCall() {
+	time.Sleep(time.Second)
+}
+
+// here context has come from outside and means that
+// we have to finish our operations in context deadline
+// because client wont wait any longer
+// and no operation after deadline makes sense
+func (h *handler) someHttpOrGrpcHandler(ctx context.Context) error {
+	rows, err := h.pool.Query(ctx, "select some_column from some_table")
+	// no error here
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var columns []int64
+	for rows.Next() {
+		var i int64
+		if err := rows.Scan(&i); err != nil {
+			return err
+		}
+		columns = append(columns, i)
+	}
+
+	if err = rows.Err(); err != nil {
+		return err
+	}
+
+	// if context is done here, error from pgx is not returned
+	// so we have to check context manually
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	// we have to check context before this function
+	// because if we wont check it and context is already canceled
+	// we would just waste our resources for nothing
+	// because client doesnt want our response already
+	someLongMathOperationOrSomeLongExternalCall()
+
+	return nil
+}
+
+func NewHandler() *handler {
 	cfg, err := pgxpool.ParseConfig("postgres://dbuser:dbpassword@localhost:5432/postgres?sslmode=disable")
 	if err != nil {
 		panic(err)
@@ -26,28 +72,10 @@ func main() {
 		}
 	}
 
-	pool, err := pgxpool.NewWithConfig(ctx, cfg)
+	pool, err := pgxpool.NewWithConfig(context.Background(), cfg)
 	if err != nil {
 		panic(err)
 	}
 
-	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*100)
-	defer cancel()
-
-	rows, err := pool.Query(ctx, "select pg_sleep(0.2)")
-	// no error here
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-	}
-
-	// still no error while it should be because context is already canceled
-	// in unchanged Handler there will be context deadline exceeded error
-	if err = rows.Err(); err != nil {
-		panic(err)
-	}
-	log.Println("finished without errors")
+	return &handler{pool: pool}
 }
